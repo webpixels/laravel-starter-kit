34593718914e26b98ab7478d8f6f62631d4e1871
Alexis <alexis.enache@gmail.com>
2022-01-22 23:20:52 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -228,6 +228,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Meta' => Eusonlito\LaravelMeta\Facade::class,
+
     ],
 
 ];
